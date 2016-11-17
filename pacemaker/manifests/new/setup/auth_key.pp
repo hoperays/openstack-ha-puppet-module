82bb4d894b2b328ac3e04da4aba3d34a1785bd55
chengql <qiliang.cheng@foxmail.com>
2016-11-17 11:07:54 +0800

    file pacemaker-auth-key ensure => $key_ensure -> link; add require file corosync-auth-key

--- pacemaker/manifests/new/setup/auth_key.pp
+++ pacemaker/manifests/new/setup/auth_key.pp
@@ -46,13 +46,13 @@ class pacemaker::new::setup::auth_key (
     mode    => '0640',
   }
 
-  file { 'pacemaker-auth-key' :
-    ensure => $key_ensure,
-    path   => '/etc/pacemaker/authkey',
-    target => '/etc/corosync/authkey',
-    owner  => $cluster_user,
-    group  => $cluster_group,
-    mode   => '0640',
+    file { 'pacemaker-auth-key':
+    ensure  => link,
+    path    => '/etc/pacemaker/authkey',
+    target  => '/etc/corosync/authkey',
+    owner   => $cluster_user,
+    group   => $cluster_group,
+    require => File['corosync-auth-key'],
   }
 
   # authkey should be placed before the cluster is created
