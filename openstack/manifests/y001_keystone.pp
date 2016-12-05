5a4c30c42aceaae2bfe9fc42ab65f23427cee746
chengql <chengql@QILIANG-THINK>
2016-12-05 22:00:13 +0800

    tuning class keystone

--- openstack/manifests/y001_keystone.pp
+++ openstack/manifests/y001_keystone.pp
@@ -1,13 +1,18 @@
 class openstack::y001_keystone (
-  $keystone_password     = 'keystone1234',
-  $host                  = 'controller-vip',
-  $cluster_nodes         = [
+  $keystone_password = 'keystone1234',
+  $host              = 'controller-vip',
+  $cluster_nodes     = [
     'controller-1',
     'controller-2',
     'controller-3'],
-  $bootstrap_node        = 'controller-1',
-  $admin_token           = 'e38f3dd7116ee3bc3dba',
-  $fernet_key_repository = '/etc/keystone/fernet-keys',) {
+  $bootstrap_node    = 'controller-1',
+  $admin_token       = 'e38f3dd7116ee3bc3dba',) {
+  if $::hostname == $bootstrap_node {
+    $enable_fernet_setup = true
+  } else {
+    $enable_fernet_setup = false
+  }
+
   class { '::keystone':
     admin_token          => $admin_token,
     rabbit_hosts         => $cluster_nodes,
@@ -19,6 +24,7 @@ class openstack::y001_keystone (
     public_bind_host     => $::hostname,
     admin_bind_host      => $::hostname,
     token_provider       => 'fernet',
+    enable_fernet_setup  => $enable_fernet_setup,
     verbose              => true,
     sync_db              => false,
     enabled              => false,
@@ -30,17 +36,7 @@ class openstack::y001_keystone (
       host          => 'controller-vip',
       allowed_hosts => $cluster_nodes,
     } ->
-    class { '::keystone::db::sync': } ->
-    exec { 'keystone-manage fernet_setup':
-      command     => 'keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone',
-      path        => '/usr/bin',
-      refreshonly => true,
-      creates     => "${fernet_key_repository}/0",
-      notify      => Anchor['keystone::service::begin'],
-      subscribe   => [Anchor['keystone::install::end'], Anchor['keystone::config::end']],
-      tag         => 'keystone-exec',
-    } ->
-    keystone_config { 'fernet_tokens/key_repository': value => $fernet_key_repository; }
+    class { '::keystone::db::sync': }
   }
 
   #  class { '::keystone::roles::admin':
