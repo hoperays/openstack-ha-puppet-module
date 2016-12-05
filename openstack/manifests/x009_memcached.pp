16ede7fbbdad86bb29a7a9a510e56a0cfe2d323a
chengql <qiliang.cheng@foxmail.com>
2016-12-05 12:58:56 +0800

    tuning memcached service

--- openstack/manifests/x009_memcached.pp
+++ openstack/manifests/x009_memcached.pp
@@ -1,12 +1,11 @@
 class openstack::x009_memcached ($bootstrap_node = 'controller-1') {
-  class { 'memcached': }
+  class { 'memcached': service_manage => false }
 
   if $::hostname == $bootstrap_node {
     pacemaker::resource::service { 'memcached':
       service_name => 'memcached',
       clone_params => true,
-      meta_params  => 'interleave=true',
-      require      => Class['memcached'],
+      require      => Class['memcached']
     }
   }
 }
