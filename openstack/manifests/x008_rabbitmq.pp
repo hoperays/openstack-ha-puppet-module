28804d227241d23d3a9c396214c020679388cca8
chengql <qiliang.cheng@foxmail.com>
2016-12-05 16:59:06 +0800

    tuning admin_enable true->false

--- openstack/manifests/x008_rabbitmq.pp
+++ openstack/manifests/x008_rabbitmq.pp
@@ -2,14 +2,15 @@ class openstack::x008_rabbitmq (
   $cluster_nodes  = ['controller-1', 'controller-2', 'controller-3'],
   $bootstrap_node = 'controller-1') {
   class { 'rabbitmq':
+    admin_enable             => false,
+    service_manage           => true,
+    service_ensure           => 'stopped',
     config_cluster           => false,
     cluster_nodes            => $cluster_nodes,
     node_ip_address          => $ipaddress_eth0,
     cluster_node_type        => 'ram',
     erlang_cookie            => 'CECDFFCEFEDBFAAECDFA',
     wipe_db_on_cookie_change => true,
-    service_manage           => true,
-    service_ensure           => 'stopped',
   }
 
   if $::hostname == $bootstrap_node {
