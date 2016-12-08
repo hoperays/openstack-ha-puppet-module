4c2b286801eed6ff0dbb04e2cddd1735c2b3c2c7
chengql <qiliang.cheng@foxmail.com>
2016-12-08 14:16:02 +0800

    delete service_name => $name

--- openstack/manifests/x007_memcached.pp
+++ openstack/manifests/x007_memcached.pp
@@ -3,7 +3,6 @@ class openstack::x007_memcached ($bootstrap_node = 'controller-1') {
 
   if $::hostname == $bootstrap_node {
     pacemaker::resource::service { 'memcached':
-      service_name => $name,
       clone_params => 'interleave=true',
       require      => Class['memcached']
     }
