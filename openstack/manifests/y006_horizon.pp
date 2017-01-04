4636c1289bd1fd458ece8f509c4b1656650c55d4
chengql <qiliang.cheng@foxmail.com>
2017-01-04 15:29:35 +0800

    tuning

--- openstack/manifests/y006_horizon.pp
+++ openstack/manifests/y006_horizon.pp
@@ -4,15 +4,31 @@ class openstack::y006_horizon (
   $secret_key     = 'd872760ce14ffd0919ad',
   $host           = 'controller-vip',) {
   class { '::horizon':
-    servername        => $::hostname,
-    server_aliases    => $::hostname,
-    allowed_hosts     => $::hostname,
-    cache_server_ip   => $cluster_nodes,
-    cache_server_port => '11211',
-    secret_key        => $secret_key,
-    keystone_url      => "http://${host}:5000",
-    django_debug      => 'True',
-    api_result_limit  => '2000',
+    bind_address          => $ipaddress_eth0,
+    servername            => $::hostname,
+    server_aliases        => $::hostname,
+    allowed_hosts         => $::hostname,
+    # listen_ssl            => true,
+    # horizon_cert          => undef,
+    # horizon_key           => undef,
+    # horizon_ca            => undef,
+    cache_server_ip       => $cluster_nodes,
+    cache_server_port     => '11211',
+    secret_key            => $secret_key,
+    keystone_url          => "http://${host}:5000",
+    django_debug          => false,
+    api_result_limit      => '2000',
+    compress_offline      => true,
+    timezone              => 'Asia/Shanghai',
+    cache_backend         => 'django.core.cache.backends.memcached.MemcachedCache',
+    django_session_engine => 'django.contrib.sessions.backends.cache',
+    neutron_options       => {
+      enable_lb        => true,
+      enable_firewall  => true,
+      enable_vpn       => true,
+      enable_ha_router => true,
+    }
+    ,
   }
 
   if $::hostname == $bootstrap_node {
