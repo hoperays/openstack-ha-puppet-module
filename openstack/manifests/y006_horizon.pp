16a3194ef95331728694dfacf78b34b47e99e61d
chengql <qiliang.cheng@foxmail.com>
2017-01-04 17:54:45 +0800

    tuning

--- openstack/manifests/y006_horizon.pp
+++ openstack/manifests/y006_horizon.pp
@@ -1,41 +1,48 @@
 class openstack::y006_horizon (
   $bind_address   = $ipaddress_eth0,
   $servername     = $::hostname,
-  $server_aliases = [$::hostname, 'controller-vip', $ipaddress_eth0, '192.168.0.130'],
+  $server_aliases = [
+    $::hostname,
+    'controller-vip',
+    $ipaddress_eth0,
+    '192.168.0.130'],
   $allowed_hosts  = [$::hostname, 'controller-vip', $ipaddress_eth0, '192.168.0.130'],
-  $cluster_nodes  = ['controller-1','controller-2','controller-3'],
+  $cluster_nodes  = [
+    'controller-1',
+    'controller-2',
+    'controller-3'],
   $secret_key     = 'd872760ce14ffd0919ad',
   $host           = 'controller-vip',
   $bootstrap_node = 'controller-1',) {
   class { '::horizon':
-    bind_address          => $bind_address,
-    servername            => $servername,
-    server_aliases        => $server_aliases,
-    allowed_hosts         => $allowed_hosts,
+    bind_address                 => $bind_address,
+    servername                   => $servername,
+    server_aliases               => $server_aliases,
+    allowed_hosts                => $allowed_hosts,
     # listen_ssl            => true,
     # horizon_cert          => undef,
     # horizon_key           => undef,
     # horizon_ca            => undef,
-    cache_server_ip       => $cluster_nodes,
-    cache_server_port     => '11211',
-    secret_key            => $secret_key,
-    keystone_url          => "http://${host}:5000/v3",
-    keystone_default_role => '_member_',
-    django_debug          => false,
-    api_result_limit      => '2000',
-    compress_offline      => true,
-    api_versions          => {
+    cache_server_ip              => $cluster_nodes,
+    cache_server_port            => '11211',
+    secret_key                   => $secret_key,
+    keystone_url                 => "http://${host}:5000/v3",
+    keystone_default_role        => '_member_',
+    django_debug                 => false,
+    api_result_limit             => '2000',
+    compress_offline             => true,
+    api_versions                 => {
       identity => '3',
       image    => '2',
       volume   => '2',
     }
     ,
-    # keystone_multidomain_support => true,
-    # keystone_default_domain      => 'default',
-    timezone              => 'Asia/Shanghai',
-    cache_backend         => 'django.core.cache.backends.memcached.MemcachedCache',
-    django_session_engine => 'django.contrib.sessions.backends.cache',
-    neutron_options       => {
+    keystone_multidomain_support => true,
+    keystone_default_domain      => 'default',
+    timezone                     => 'Asia/Shanghai',
+    cache_backend                => 'django.core.cache.backends.memcached.MemcachedCache',
+    django_session_engine        => 'django.contrib.sessions.backends.cache',
+    neutron_options              => {
       enable_lb        => true,
       enable_firewall  => true,
       enable_vpn       => true,
@@ -48,6 +55,7 @@ class openstack::y006_horizon (
     exec { 'apache-restart':
       command     => "/usr/sbin/pcs resource restart apache-clone",
       refreshonly => true,
+      subscribe   => Class['::horizon'],
     }
   }
 }
