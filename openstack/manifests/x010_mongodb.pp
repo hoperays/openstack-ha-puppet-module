dfa2997de6df5021511c3af3f59021596bb0277e
chengql <qiliang.cheng@foxmail.com>
2017-02-10 13:11:24 +0800

    tuning

--- openstack/manifests/x010_mongodb.pp
+++ openstack/manifests/x010_mongodb.pp
@@ -3,10 +3,7 @@ class openstack::x010_mongodb (
   $controller_1   = '192.168.0.131',
   $controller_2   = '192.168.0.132',
   $controller_3   = '192.168.0.133',) {
-  class { '::mongodb::globals':
-    manage_package_repo => false,
-    manage_package      => true,
-  } ->
+  class { '::mongodb::globals': manage_package_repo => false, } ->
   class { '::mongodb::server':
     bind_ip    => ['127.0.0.1', $ipaddress_eth0],
     replset    => 'ceilometer',
